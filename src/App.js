6a413b94b843b327392da7c8d7d1929ecb7f5b1d
doublea85 <aalroux85@gmail.com>
2022-10-01 00:07:05 +0200

    Some features

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Search from './componets/search/Search';
 import CurrentWeather from './componets/current-weather/CurrentWeather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
 
   const handleOnSearchChange = (searchData) =>{
-    console.log(searchData);
+    /** Nous alons rechercher des donées via la logitude et la latitude
+     * Transformer les deux données en tableau puis le injecter dans l'ur que nous utilisons dan fetch
+     */
+     const [lat, lon] = searchData.value.split(" ");
+
+     // Ici nous allons mettre le début de l'url dans le composant/fichier api puis l'importer comme on l'a fait avec le composant currentweather la pertie paramètre reste ici
+
+     // Request for the current weather
+     const currentWeatherFetch = fetch(
+       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+     );
+
+     // Request for the forcast
+     const forcastFetch = fetch(
+       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+     );
+
+     Promise.all([])
   }
   return (
     <div className="container">
--- src/api.js
+++ src/api.js
@@ -10,5 +10,9 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+// La clé de l'api
+export const WEATHER_API_KEY = "6266f2b31bfc1b45d1af81c1ca860a07";
+
 
 
